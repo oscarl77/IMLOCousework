054ccea858cafd4f2c03dda49cf5b1ba9cfbd7a9
oscarl77 <oscarlaurens18@gmail.com>
2025-05-01 00:28:36 +0100

    Moved training loop code into separate method in train.py

--- src/train.py
+++ src/train.py
@@ -1,36 +1,46 @@
 import torch
 
+from src.validate import validate_model
+
+def train_loop(model, epochs, loss_fn, optimizer, train_loader, val_loader):
+    train_losses, train_accuracies = [], []
+    val_losses, val_accuracies = [], []
+
+    for epoch in range(epochs):
+        train_loss, train_accuracy = train_model(model, loss_fn, optimizer, train_loader)
+        val_loss, val_accuracy = validate_model(model, loss_fn, val_loader)
+
+        train_losses.append(train_loss)
+        train_accuracies.append(train_accuracy)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_accuracy)
+
+        print(f'Epoch {epoch}, Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
+
+    return train_losses, train_accuracies, val_losses, val_accuracies
+
 def train_model(model, loss_fn, optimizer, train_loader):
     model.train()
-    avg_train_loss = 0
     running_loss = 0.0
-    correct_train = 0
-    total_train = 0
+    correct = 0
+    total = 0
 
     for inputs, labels in train_loader:
-        # zero the parameter gradients
         optimizer.zero_grad()
-
-        # forward pass
         outputs = model(inputs)
-
-        # compute loss
         loss = loss_fn(outputs, labels)
-
-        # backward pass with optimisation
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
 
         # obtain model's predicted class label
         _, predicted = torch.max(outputs, 1)
-        total_train += labels.size(0)
-        correct_train += (predicted == labels).sum().item()
+
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
     # calculate average loss per batch
     avg_train_loss = running_loss / len(train_loader)
-
-    # calculate training accuracy
-    train_accuracy = (correct_train / total_train) * 100
+    train_accuracy = (correct / total) * 100
 
     return avg_train_loss, train_accuracy
