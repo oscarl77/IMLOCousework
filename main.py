425ba7353f6e822c0a28a032d17e1e9f0c3dff6d
oscarl77 <oscarlaurens18@gmail.com>
2025-04-30 23:35:25 +0100

    Implemented validation

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import numpy as np
 
 from src.models.model import CNNModel
 from src.utils.data_loader import get_data_loaders
-from src.train import train
+from src.train import train_model
+from src.utils.graphing import plot_loss_and_accuracies
+from src.validate import validate_model
 
 # set seeds to compare model performance across training runs
 torch.manual_seed(0)
@@ -33,7 +35,21 @@ def main():
     # Use adam optimizer
     adam_optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    train(model, epochs, loss_fn, adam_optimizer, train_loader)
+    train_losses, train_accuracies = [], []
+    val_losses, val_accuracies = [], []
+
+    for epoch in range(epochs):
+        train_loss, train_accuracy = train_model(model, loss_fn, adam_optimizer, train_loader)
+        val_loss, val_accuracy = validate_model(model, loss_fn, val_loader)
+
+        train_losses.append(train_loss)
+        train_accuracies.append(train_accuracy)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_accuracy)
+
+        print(f'Epoch {epoch}, Training Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
+
+    plot_loss_and_accuracies(epochs, train_losses, train_accuracies)
 
 if __name__ == '__main__':
     main()
