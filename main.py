54641197d72642dbd3d4d175def1bd654765c967
oscarl77 <oscarlaurens18@gmail.com>
2025-05-11 22:52:43 +0100

    Training new model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import random
 import numpy as np
 
+from src.models.model_v2 import CNNModelV2
 from src.models.model import CNNModel
 from src.train import train_loop
 from src.utils.data_loader import get_data_loaders
@@ -9,14 +10,14 @@ from src.utils.experiment_logger import log_experiment_details
 from src.utils.graphing import plot_loss_and_accuracies
 
 # set fixed seeds to compare model performance across training runs
-torch.manual_seed(40)
-random.seed(40)
-np.random.seed(40)
+torch.manual_seed(42)
+random.seed(42)
+np.random.seed(42)
 
 #torch.set_num_threads(1)
 
 def main():
-    experiment_name = 'CNN_v0.10'
+    experiment_name = 'CNN_v1.00'
     print(f"----------------{experiment_name}----------------")
 
     # ensure cpu usage
@@ -25,20 +26,23 @@ def main():
     # define hyperparameters
     batch_size = 128
     subset_size = None
-    epochs = 30
-    learning_rate = 0.002
+    epochs = 10
+    learning_rate = 0.001
 
     log_experiment_details(experiment_name, subset_size, batch_size, learning_rate,
-                           data_augmentation="Horizontal flipping, Cropping", regularisation="1 dropout layer of 0.2",
-                           conv_layers="4", fc_layers="2", additional_notes="Stride of 2 to conv 2 and 4,"
-                                                                            "conv2 pooling,"
-                                                                            "batch normalization")
+                           data_augmentation="Horizontal flipping, Cropping, Rotation and ColorJitter",
+                           regularisation="None",
+                           conv_layers="6",
+                           fc_layers="1",
+                           additional_notes="Max pooling every 2 layers,"
+                                            "batch normalization,"
+                                            "Learning rate: 0.001")
 
     # load datasets
     train_loader, val_loader, test_loader = get_data_loaders(batch_size=batch_size, subset_size=subset_size)
 
     # load model
-    model = CNNModel().to(device)
+    model = CNNModelV2().to(device)
 
     # Use cross entropy loss function
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -46,7 +50,7 @@ def main():
     # Use adam optimizer
     adam_optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=adam_optimizer, mode='min', factor=0.3, patience=3)
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=adam_optimizer, mode='min', factor=0.5, patience=3)
 
     # Run training loop for model training and validation
     train_losses, train_accuracies, val_losses, val_accuracies = train_loop(
