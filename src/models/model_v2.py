5e765280c1fbc9474ece0349938573bc2bb4cd93
oscarl77 <oscarlaurens18@gmail.com>
2025-05-14 14:35:04 +0100

    Increased depth, used GELU activation func

--- src/models/model_v2.py
+++ src/models/model_v2.py
@@ -9,32 +9,56 @@ class CNNModelV2(nn.Module):
         self.block1 = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1, stride=2),
+            nn.GELU(),
+            nn.Conv2d(32, 32, kernel_size=3),
             nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
+            nn.GELU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.1),
         )
 
         self.block2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+            nn.GELU(),
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.GELU(),
+            nn.Dropout(0.2),
+        )
+
+        self.block3 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
+            nn.GELU(),
+            nn.Conv2d(128, 128, kernel_size=3),
+            nn.BatchNorm2d(128),
+            nn.GELU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.2),
+        )
+
+        self.block4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.GELU(),
+            nn.Conv2d(256, 256, kernel_size=3),
+            nn.BatchNorm2d(256),
+            nn.GELU(),
+            nn.Dropout(0.3)
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(128 * 4 * 4, 512),
+            nn.Linear(256 * 3 * 3,2304),
             nn.ReLU(inplace=True),
-            nn.Linear(512, output_features),
+            nn.Linear(2304, output_features),
         )
 
     def forward(self, x):
         x = self.block1(x)
         x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
